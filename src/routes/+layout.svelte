66a00bcc23d8a45b7e52a632fe8abe9edb5b9f41
44 <44x1carbon@gmail.com>
2023-01-24 19:27:39 +0900

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script>
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from 'firebase/app';
+	import { getAnalytics } from 'firebase/analytics';
+	// TODO: Add SDKs for Firebase products that you want to use
+	// https://firebase.google.com/docs/web/setup#available-libraries
+
+	onMount(() => {
+		// Your web app's Firebase configuration
+		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		const firebaseConfig = {
+			apiKey: 'AIzaSyDJiJ7iOX4zQ6l4wH89G1R-vQu5xZ6KAHE',
+			authDomain: 'genso-tools.firebaseapp.com',
+			projectId: 'genso-tools',
+			storageBucket: 'genso-tools.appspot.com',
+			messagingSenderId: '1021494508287',
+			appId: '1:1021494508287:web:283adc5a6e81ea4a54eaca',
+			measurementId: 'G-2YTL0DQ3WQ'
+		};
+
+		// Initialize Firebase
+		const app = initializeApp(firebaseConfig);
+		const analytics = getAnalytics(app);
+	});
 </script>
 
 <header class="block border-box text-white header">
