42228a504c4f524f8aa43aef7f8b20615a8a16cb
44 <44x1carbon@gmail.com>
2023-01-28 22:03:02 +0900

    update

--- src/routes/wantedparty/+page.svelte
+++ src/routes/wantedparty/+page.svelte
@@ -33,4 +33,8 @@
 			<WantedPartyInfoCard {wantedParty} />
 		</div>
 	{/each}
+
+	{#if $wantedPartyListStore.length}
+		<div class="panel border border-well-read-700 p-2 w-full">募集中のPTが見つかりませんでした</div>
+	{/if}
 </div>
