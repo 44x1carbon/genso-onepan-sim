170518d439db589397768c846e6934178d569705
44 <44x1carbon@gmail.com>
2023-01-29 10:53:19 +0900

    update

---
+++ src/routes/wantedparty/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<meta property="og:title" content="元素騎士 PT募集掲示板" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={`https://ignis-tools.vercel.app/wantedparty/${data.id}`} />
+	<meta
+		property="og:image"
+		content={`https://ignis-tools.vercel.app/wantedparty/image?id=${data.id}`}
+	/>
+	<meta property="og:site_name" content="元素騎士 PT募集掲示板" />
+	<meta property="og:description" content="元素騎士オンラインのPT募集掲示板です。" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@44genso" />
+	<meta name="twitter:player" content="@44genso" />
+</svelte:head>
+
+{data.id}
