a4dcdbd2d4138871b17d26ecae06443a370c00a4
44 <44x1carbon@gmail.com>
2023-02-07 01:03:50 +0900

    update

--- src/routes/wantedparty/[id]/+page.ts
+++ src/routes/wantedparty/[id]/+page.ts
@@ -1,10 +1,16 @@
-import type { Load } from '@sveltejs/kit';
+import { redirect, type Load } from '@sveltejs/kit';
 import useFirestore from '$lib/wantedparty/Firestore';
+import { goto } from '$app/navigation';
 
 export const load: Load = async ({ params }) => {
     const firestore = useFirestore();
-    const wantedParty = await firestore.getWantedParty(params.id);
-    return {
-        wantedParty
-    };
+
+    try {
+        const wantedParty = await firestore.getWantedParty(params.id);
+        return {
+            wantedParty
+        };
+    } catch {
+        throw redirect(302, "/wantedparty")
+    }
 };
